a240fc3df8887ecffe175a4424930ab347a31868
Banzile <banzile.nhlebela74@gmail.com>
2025-05-05 13:16:12 +0200

    changed git ignore

--- client/dist/assets/index-C26mI1oU.js
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:d7dc6b0d961c5e4074733c84cf9f0831c3566f7ff02328846cbf055aa183a3d4
-size 1329864
---
+++ client/dist/entries/index-B1dvSIPA.js
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:7ea9be42bcba70c4c7191d7330b6aec456b06a23b2d6e5de651c38ab254a506b
+size 1309859
